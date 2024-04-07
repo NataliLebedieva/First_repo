4c4e449b3602ac25adfb466b558f25f6e0fedf8b
NataliLebedieva <4155672@gmail.com>
2024-04-07 22:26:45 +0300

    mod1

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-name = input("Введіть своє ім'я:")
-print(f"Hello {name}!")
-name(type)
+# all_sec = 5000
+# hours = all_sec // 3600
+# print(hours)
+# minutes = (all_sec - hours * 3600)//60
+# print(minutes)
+# seconds = (all_sec - (hours * 3600) - (minutes*60))
+# print(seconds)
+# print(f"{all_sec} секунд, це {hours} година, {minutes} хвилини та {seconds} секунд.")
+# print(f"годин: {hours}, хвилин: {minutes} секунд:{seconds}")
+# print(type(seconds))
+# print(f"години менше секунд: {bool(hours < seconds)}")
+# printa = 1
+# print(f"змінна printa {bool(printa)}")
+# age1 = 18
+# age2 = 20
+# print(f"{age1} більше {age2}: {bool({age1} > {age2})}")
+# a = float(input("Введіть сторону квадрата a: "))
+# p = a * 4
+# print(f"Периметр квадрата: {p}")
+# a = 15.0
+# b = 2
+# print(a//b)
