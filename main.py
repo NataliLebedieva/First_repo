21b701100e51739ac0fe8576eeae0fbeccf528e3
NataliLebedieva <4155672@gmail.com>
2024-04-13 00:24:13 +0300

    com5

--- main.py
+++ main.py
@@ -20,3 +20,91 @@
 # a = 15.0
 # b = 2
 # print(a//b)
+# my_list = list()
+# my_list = ["Natali",2,3]
+# print(f"початковий список індекс 2 або -1: {my_list[-1]}")
+# my_list.append(5)
+# print(f"список з додаванням елементу в кінець: {my_list}")
+# my_list[-1] = 6
+# print(f"список зі зміною елементу 3 чи -1: {my_list}")
+# print(f"видалення 0: {my_list.pop(0)}")
+# print(f"видалення 0 список : {my_list}")
+# my_list_2 = ["Nata", "Lebed"]
+# my_list.extend(my_list_2)
+# print(f"список після розширення списку: {my_list}")
+# my_list.insert(0, 1)
+# print(f"список після вставлення елементу '1' на 0 позицію: {my_list}")
+# # my_list.clear()
+# # print(f"список очищений: {my_list}")
+# print(f"індекс '3': {my_list.index(3)}")
+# print((f"кількість '2': {my_list.count(2)}"))
+
+# print(f"довжина списку: {len(my_list)}")
+# while True:
+#     user_input = input()
+#     print(user_input)
+#     if user_input == "0":
+#         break
+# a = 0
+# while a < 6:
+#     a = a + 1
+#     if a % 2:
+#         continue
+#     print(a)
+
+# for i in range(1, 10):
+#     if i % 2 == 0:
+#         print(f'{i} є парним числом.')
+#     else:
+#         print(f'{i} є непарним числом.')
+
+# while True:
+#     number = input("number = ")
+#     number = int(number)
+#     while True:
+#         print(number)
+#         number = number - 1
+#         if number < 0:
+#             break
+
+
+# is_next = None
+# num = int(input("Enter the number of points: "))
+# if num >= 83:
+#     is_next = True
+#     print("кандидат проходить до наступного туру")
+# else:
+#     is_next = False
+#     print("кандидат не проходить до наступного туру")
+
+
+# work_experience = int(input("Enter your full work experience in years: "))
+
+# if work_experience > 1 and work_experience <= 5:
+#     developer_type = "Middle"
+# elif work_experience <= 1:
+#     developer_type = "Junior"
+# else:
+#     developer_type = "Senior"
+# print(f"Категорія розробника {developer_type}")
+
+
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     if num % 2 == 1:
+#         result = "Positive odd number"
+#     else:
+#         result = "Positive even number"
+# elif num < 0:
+#     result = "Negative number"
+# else:
+#     result = "It is zero"
+# print(result)
+
+num = int(input("Enter the integer (0 to 100): "))
+sum = num
+while num >= 0:
+    sum += num - 1
+print(sum)
+
